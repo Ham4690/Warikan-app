0d59586f87197dd4a3bf392c2f67f3598f151096
Ham4690 <ham46903@gmail.com>
2020-03-06 17:13:08 +0900

    Updates

---
+++ precache-manifest.0b2ab07e82c736605901427cad9efe28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea3d521618bab221613d170f0b1a0717",
+    "url": "/Warikan-app/index.html"
+  },
+  {
+    "revision": "eaeec5668ad3d6c65c0c",
+    "url": "/Warikan-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b396ab3b36aa1673c56a",
+    "url": "/Warikan-app/static/js/2.720d9d91.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Warikan-app/static/js/2.720d9d91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaeec5668ad3d6c65c0c",
+    "url": "/Warikan-app/static/js/main.6a086221.chunk.js"
+  },
+  {
+    "revision": "ad1cae0861a98eaeb9ad",
+    "url": "/Warikan-app/static/js/runtime-main.0d740eef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Warikan-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
